0c1575526d0a890fb50a7f213be52cfa721bdc31
Prabir Singh <codeprabir@gmail.com>
2022-10-04 13:25:34 +0530

    bug fixing pending

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
 const container = document.querySelector('.container')
-const seat = document.querySelectorAll('.row.seat:not(.occupied)')
+const seats = document.querySelectorAll('.row.seat:not(.occupied)')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 const movieSelect = document.getElementById('movie')
+
+populateUi()
 let ticketPrice = +movieSelect.value
+
+//save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+//get data from localstorage and populate ui
+
+function populateUi() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex')
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
+}
 
 //Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
 
   const selectedSeatsCount = selectedSeats.length
   count.innerText = selectedSeatsCount
@@ -17,6 +46,7 @@ function updateSelectedCount() {
 //movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value
+  setMovieData(e.target.selectedIndex, e.target.value)
   updateSelectedCount()
 })
 
@@ -30,3 +60,7 @@ container.addEventListener('click', (e) => {
     updateSelectedCount()
   }
 })
+
+//Initial count and total set
+
+updateSelectedCount()
